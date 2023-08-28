82317e57e59fe2fa107a0b4f62a73fef8870dac1
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-08-29 00:33:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const excuses = [
 ];
 
 const display = document.getElementById("display");
+const copyButton = document.getElementById("copy-button");
 const spinButton = document.getElementById("spin-button");
 
 spinButton.addEventListener("click", () => {
   spinButton.disabled = true;
+  copyButton.disabled = true;
   const randomExcuse = excuses[Math.floor(Math.random() * excuses.length)];
 
-  display.innerHTML = `<p>${randomExcuse}</p>`;
+  display.innerHTML = `<p><span class="highlight">SmerfTV_</span>${randomExcuse}</p>`;
 
   setTimeout(() => {
     spinButton.disabled = false;
+    copyButton.disabled = false;
   }, 1000);
 });
 
+copyButton.addEventListener("click", () => {
+  const textToCopy = display.textContent;
+  const textArea = document.createElement("textarea");
+  textArea.value = textToCopy;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+});
