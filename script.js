e632b11eb2b674ad25c5bf17e39f0a3412e9d5cc
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-08-29 00:29:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
 const excuses = [
   "Nie mam dzisiaj czasu.",
   "Techniczne problemy.",
-  "Nie czuję się najlepiej.",
-  "Niestety, coś mi wyskoczyło.",
-  "Chciałbym, ale muszę pracować.",
-  "Rodzinne zobowiązania.",
-  "Planuję coś innego.",
-  "Internet mi dzisiaj szwankuje.",
-  "Nie mam dobrego oświetlenia.",
-  "Chcę odpocząć po ciężkim dniu.",
-  "Sprzęt nie działa jak trzeba.",
-  "Wymagany odpoczynek.",
-  "Brak dobrego pomysłu na dzisiejszy stream.",
-  "Muszę się przygotować lepiej.",
-  "Wracam za tydzień!",
-  "Awaria prądu.",
-  "Nie mogę dzisiaj, bo remontują sąsiadów.",
-  "Zapomniałem zaplanować stream.",
-  "Odpoczywam od streamowania.",
-  "Szukam nowych gier do zaprezentowania."
+  // Dodaj więcej wymówek...
 ];
 
 const display = document.getElementById("display");
 const spinButton = document.getElementById("spin-button");
 
-function spin() {
+spinButton.addEventListener("click", () => {
   spinButton.disabled = true;
   const randomExcuse = excuses[Math.floor(Math.random() * excuses.length)];
 
-  let currentExcuseIndex = 0;
-  const interval = setInterval(() => {
-    display.textContent = excuses[currentExcuseIndex];
-    currentExcuseIndex++;
+  display.innerHTML = `<p>${randomExcuse}</p>`;
 
-    if (currentExcuseIndex >= excuses.length) {
-      clearInterval(interval);
-      display.textContent = randomExcuse;
-      spinButton.disabled = false;
-    }
-  }, 100);
-}
+  setTimeout(() => {
+    spinButton.disabled = false;
+  }, 1000);
+});
+
