540c69321cce14985fc6fe8bba0208388ae2fb69
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-08-29 02:30:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-const reelInner = document.getElementById("reelInner");
-const spinButton = document.getElementById("spinButton");
-const nextButton = document.getElementById("nextButton");
+document.addEventListener("DOMContentLoaded", () => {
+  const reelInner = document.getElementById("reelInner");
+  const spinButton = document.getElementById("spinButton");
+  const nextButton = document.getElementById("nextButton");
 
-const excuses = [
-  "Przewidziane przerwy techniczne.",
-  "Pilne zobowiązania rodzinne.",
-  "Nieoczekiwana choroba.",
-  "Nagłe problemy z internetem.",
-  "Wizyta niezapowiedzianych gości.",
-  "Wymagająca sesja treningowa.",
-  "Konieczność załatwienia pilnych spraw.",
-  "Planowane aktualizacje sprzętu.",
-  "Udział w konferencji lub warsztatach.",
-  "Potrzeba odpoczynku i relaksu."
-];
+  const excuses = [
+    "Przewidziane przerwy techniczne.",
+    "Pilne zobowiązania rodzinne.",
+    "Nieoczekiwana choroba.",
+    "Nagłe problemy z internetem.",
+    "Wizyta niezapowiedzianych gości.",
+    "Wymagająca sesja treningowa.",
+    "Konieczność załatwienia pilnych spraw.",
+    "Planowane aktualizacje sprzętu.",
+    "Udział w konferencji lub warsztatach.",
+    "Potrzeba odpoczynku i relaksu."
+  ];
 
-let isSpinning = false;
+  let isSpinning = false;
 
-function getRandomExcuse() {
-  return excuses[Math.floor(Math.random() * excuses.length)];
-}
+  function getRandomExcuse() {
+    return excuses[Math.floor(Math.random() * excuses.length)];
+  }
 
-function spinReel() {
-  if (isSpinning) return;
+  function spinReel() {
+    if (isSpinning) return;
 
-  isSpinning = true;
-  spinButton.disabled = true;
-  nextButton.classList.add("hidden");
+    isSpinning = true;
+    spinButton.disabled = true;
+    nextButton.classList.add("hidden");
 
-  const spinDuration = 5000;
-  const startPosition = 0;
-  const endPosition = -600;
+    const spinDuration = 5000;
+    const startPosition = 0;
+    const endPosition = -600;
 
-  reelInner.style.animation = `scroll ${spinDuration}ms linear`;
-  reelInner.style.transform = `translateY(${endPosition}px)`;
+    reelInner.style.animation = `scroll ${spinDuration}ms linear`;
+    reelInner.style.transform = `translateY(${endPosition}px)`;
 
-  setTimeout(() => {
-    isSpinning = false;
-    spinButton.disabled = false;
-    nextButton.classList.remove("hidden");
+    setTimeout(() => {
+      isSpinning = false;
+      spinButton.disabled = false;
+      nextButton.classList.remove("hidden");
 
-    reelInner.style.animation = "";
-    reelInner.style.transform = `translateY(${startPosition}px)`;
-  }, spinDuration);
-}
+      reelInner.style.animation = "";
+      reelInner.style.transform = `translateY(${startPosition}px)`;
+    }, spinDuration);
+  }
 
-spinButton.addEventListener("click", spinReel);
+  spinButton.addEventListener("click", spinReel);
 
-nextButton.addEventListener("click", () => {
-  const excuse = getRandomExcuse();
-  reelInner.innerHTML = `<p>${excuse}</p>`;
+  nextButton.addEventListener("click", () => {
+    const excuse = getRandomExcuse();
+    reelInner.innerHTML = `<p>${excuse}</p>`;
+    nextButton.classList.add("hidden");
+  });
+
   nextButton.classList.add("hidden");
 });
-
-nextButton.classList.add("hidden");
-</body>
