b0eefd33d00676c0a745c109f574b66b874a7223
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-08-29 02:46:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const reelInner = document.getElementById("reelInner");
-  const spinButton = document.getElementById("spinButton");
-  const nextButton = document.getElementById("nextButton");
+const excuses = [
+  "Przepraszam, ale dzisiaj nie mam wystarczająco dużo czasu na stream.",
+  "Mam techniczne problemy, które muszę naprawić przed streamem.",
+  // Dodaj kolejne wymówki tutaj
+];
 
-  const excuses = [
-    "Przewidziane przerwy techniczne.",
-    "Pilne zobowiązania rodzinne.",
-    "Nieoczekiwana choroba.",
-    "Nagłe problemy z internetem.",
-    "Wizyta niezapowiedzianych gości."
-  ];
+const excuseDisplay = document.getElementById("excuseDisplay");
+const generateButton = document.getElementById("generateButton");
+const nextButton = document.getElementById("nextButton");
 
-  let isSpinning = false;
+let currentExcuseIndex = -1;
 
-  function getRandomExcuse() {
-    return excuses[Math.floor(Math.random() * excuses.length)];
-  }
+generateButton.addEventListener("click", () => {
+  currentExcuseIndex = Math.floor(Math.random() * excuses.length);
+  excuseDisplay.textContent = excuses[currentExcuseIndex];
+  nextButton.disabled = false;
+});
 
-  function spinReel() {
-    if (isSpinning) return;
-
-    isSpinning = true;
-    spinButton.disabled = true;
-    nextButton.classList.add("hidden");
-
-    const spinDuration = 5000;
-    const startPosition = 0;
-    const endPosition = -600;
-
-    reelInner.style.animation = `scroll ${spinDuration}ms linear`;
-    reelInner.style.transform = `translateY(${endPosition}px)`;
-
-    setTimeout(() => {
-      isSpinning = false;
-      spinButton.disabled = false;
-      nextButton.classList.remove("hidden");
-
-      reelInner.style.animation = "";
-      reelInner.style.transform = `translateY(${startPosition}px)`;
-    }, spinDuration);
-  }
-
-  spinButton.addEventListener("click", spinReel);
-
-  nextButton.addEventListener("click", () => {
-    const excuse = getRandomExcuse();
-    reelInner.innerHTML = `<p>${excuse}</p>`;
-    nextButton.classList.add("hidden");
-  });
-
-  nextButton.classList.add("hidden");
+nextButton.addEventListener("click", () => {
+  currentExcuseIndex = (currentExcuseIndex + 1) % excuses.length;
+  excuseDisplay.textContent = excuses[currentExcuseIndex];
 });
 
