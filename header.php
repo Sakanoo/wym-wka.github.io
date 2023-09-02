d1a6231cde142ab897df36f5c1fda95c888f56a9
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-09-03 00:13:57 +0200

    Update and rename gtag.js to header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<head>
+  
+</head><script async src="https://www.googletagmanager.com/gtag/js?id=G-EC2MGM3WG0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EC2MGM3WG0');
+</script>
+<head>
