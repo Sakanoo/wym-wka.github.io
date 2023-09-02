11f7ebc473b10155a575c8fda25adf930a9d0d73
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-09-03 00:12:54 +0200

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,8 +1,1 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EC2MGM3WG0"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-EC2MGM3WG0');
-</script>
