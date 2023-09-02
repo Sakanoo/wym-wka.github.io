fc04d89ce061662f0ec0ae6f7b8e6efa6e412ff8
Sakano <77728928+Sakanoo@users.noreply.github.com>
2023-09-03 00:26:20 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <head>
   
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EC2MGM3WG0"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LQ4G2MS3SX"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-EC2MGM3WG0');
+  gtag('config', 'G-LQ4G2MS3SX');
 </script>
 <head>
